d5fa773312c00dbf9580a8f3f0459ef5d881bea8
Eldin Alibasic <103897037+Eldoo12@users.noreply.github.com>
2022-04-17 19:29:43 +0200

    Add files via upload

---
+++ sys/Simple Login FORM/LoginForm.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Reflection;
+
+namespace Simple_Login_FORM
+{
+    public partial class LoginForm : Form
+    {
+        MySqlConnection con = new MySqlConnection("datasource = localhost; database=login_register_sys;port=3306; username = root; password= root");
+        int i;
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RegisterButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            RegisterForm fm = new RegisterForm();
+            fm.Show();
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            i = 0;
+            con.Open();
+            MySqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT * from users where username='" + UsernameBox.Text + "' and pass ='" + PasswordBox.Text + "'";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            da.Fill(dt);
+            i = Convert.ToInt32(dt.Rows.Count.ToString());
+
+            if (i == 0)
+            {
+                MessageBox.Show("Pogresna sifra ili password");
+            }
+            else
+            {
+                this.Hide();
+                Main fm = new Main();
+                fm.Show();
+            }
+            con.Close();
+        }
+    }
+}
---
+++ sys/Simple Login FORM/RegisterForm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Simple_Login_FORM
+{
+    public partial class RegisterForm : Form
+    {
+        MySqlConnection con = new MySqlConnection("datasource = localhost; database=login_register_sys;port=3306; username = root; password= root");
+        public RegisterForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ReturnButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            LoginForm fm = new LoginForm();
+            fm.Show();
+        }
+
+        private void CreateButton_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            MySqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "INSERT INTO users (email,username, pass) VALUES ('" + EmailBox.Text + "', '" + PasswordBox.Text + "','" + UsernameBox.Text + "');";
+            DataTable de = new DataTable();
+            MySqlDataAdapter dp = new MySqlDataAdapter(cmd);
+            dp.Fill(de);
+            con.Close();
+            MessageBox.Show("Uspjesno ste registrovani");
+        }
+
+        private void RegisterForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
